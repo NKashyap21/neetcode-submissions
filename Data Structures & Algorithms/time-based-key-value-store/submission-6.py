05cf7e92e3444ac7bcca84572d9986fa24885c2c
Kashyap Nukala <nukalakashyap21@gmail.com>
2026-06-14 19:48:53 +0530

    Add: time-based-key-value-store - submission-6

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-6.py
@@ -0,0 +1,31 @@
+class TimeMap:
+
+    def __init__(self):
+        self.my_hash = {}        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key in self.my_hash:
+            self.my_hash[key][0].append(value)
+            self.my_hash[key][1].append(timestamp)
+        else:
+            self.my_hash[key] = [[value],[timestamp]]
+
+    def get(self, key: str, timestamp: int) -> str:
+        values,timestamps = self.my_hash.get(key,[[],[]])
+        if values == []:
+            return ""
+        
+        l,r = 0,len(timestamps)-1
+        while l<=r:
+            m = (l+r)//2
+            if timestamps[m] == timestamp:
+                return values[m]
+            elif timestamps[m] < timestamp:
+                l = m+1
+            else:
+                r = m-1
+        if timestamps[r] > timestamp:
+            return ""
+        else:
+            return values[r]
+
