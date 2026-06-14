2e466d6c33867114787333c9c150990d12159794
Kashyap Nukala <nukalakashyap21@gmail.com>
2026-06-14 21:18:55 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+        myHash = {"(":")","[":"]","{":"}"}
+        stack = []
+        for i in range(len(s)):
+            if len(stack) == 0:
+                stack.append(s[i])
+                continue
+            
+            if s[i] == myHash.get(stack[-1],""):
+                stack.pop()
+            else:
+                stack.append(s[i])
+        
+        return len(stack)==0
